5e2f2212a26534acdab75920b02495813f7dcacc
jeffrey k eliasen <jeff@jke.net>
2017-03-26 09:16:45 -0800

    put metrics functions into separate module

---
+++ functions.py
@@ -0,0 +1,17 @@
+import datetime
+
+from django.contrib.auth.models import AnonymousUser
+
+from .models import Access
+
+
+def access_entity(session, user, entity):
+    if isinstance(user, AnonymousUser):
+        user = None
+    return Access.objects.create(
+        timestamp=datetime.datetime.now(),
+        session_key=session.session_key,
+        user=user if user else None,
+        action='access',
+        resource='{0}:{1}'.format(entity._meta.db_table, entity.pk),
+    )
