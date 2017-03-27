71860e6af1a362421443bdb1847c42b7afb061d8
jeffrey k eliasen <jeff@jke.net>
2017-03-26 18:37:35 -0800

    log rather than print

--- middleware.py
+++ middleware.py
@@ -1,18 +1,24 @@
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
 class UserSessionTrackingMiddleware(object):
 
     def process_request(self, request):
         request.pmetrics_key = request.session.session_key if hasattr(request, 'session') else None
 
     def process_response(self, request, response):
-        print 'RESPONSE ->',
         if hasattr(request, 'user'):
             request_key = request.pmetrics_key
             if request.session.session_key is None:
                 request.session.cycle_key()
             response_key = request.session.session_key
-            print 'USER:', request.user,
-            print 'KEY:', response_key,
-            print 'CHANGED:', request_key != response_key
-        else:
-            print 'STATUS:', response.status_code
+            if request_key != response_key:
+                log.info('user {0} session key changed from {1}... to {2}'.format(
+                    request.user,
+                    request_key[:5] if request_key else '<none>',
+                    response_key[:5],
+                ))
         return response
