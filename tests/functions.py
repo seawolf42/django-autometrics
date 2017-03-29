d75653bf68805c1cf2ccf840e28838f477f4b879
jeffrey k eliasen <jeff@jke.net>
2017-03-29 07:36:10 -1000

    refactor: extract test module

---
+++ tests/functions.py
@@ -0,0 +1,42 @@
+import datetime
+
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.test.client import Client
+
+from ..functions import access_entity
+
+from ..models import Access
+
+
+class AccessEntityTest(TestCase):
+
+    def setUp(self):
+        self.entity = get_user_model().objects.create(username='entity')
+        self.session = Client().session
+
+    def _test_access_state(self, expected_user):
+        time_before = datetime.datetime.now()
+        access_entity(self.session, self.user, self.entity)
+        time_after = datetime.datetime.now()
+        self.assertEqual(Access.objects.count(), 1)
+        access = Access.objects.get()
+        self.assertTrue(time_before <= access.timestamp <= time_after)
+        self.assertEqual(access.session_key, self.session.session_key)
+        self.assertEqual(access.user, expected_user)
+        self.assertEqual(access.action, 'access')
+        self.assertEqual(access.resource, '{0}:{1}'.format(self.entity._meta.db_table, self.entity.pk))
+
+    def test_access_entity(self):
+        self.user = get_user_model().objects.create(username='user')
+        self._test_access_state(self.user)
+
+    def test_access_entity_no_user(self):
+        self.user = None
+        self._test_access_state(None)
+
+    def test_access_entity_anon_user(self):
+        self.user = AnonymousUser()
+        self._test_access_state(None)
--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,2 @@
+from .functions import *     # noqa
 from .models import *        # noqa
