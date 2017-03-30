84ae9357da099619e6903e2fd809e31b8682ba86
jeffrey k eliasen <jeff@jke.net>
2017-03-29 22:23:10 -1000

    tests pass

--- djangae_rest_autometrics/models.py
+++ djangae_rest_autometrics/models.py
@@ -1,13 +1,14 @@
 import logging
 
+from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_in
 from django.contrib.sessions.models import Session
 from django.db import models
 
-from accounts.models import User
-
 import settings
 
+
+USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', User)
 
 log = logging.getLogger(__name__)
 
@@ -15,7 +16,7 @@ log = logging.getLogger(__name__)
 class MetricsModel(models.Model):
 
     class Meta:
-        app_label = 'metrics'
+        app_label = 'djangae_rest_autometrics'
         abstract = True
 
 
@@ -24,7 +25,7 @@ class Access(MetricsModel):
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
 
     session_key = models.CharField(max_length=100, editable=False)
-    user = models.ForeignKey(User, null=True, editable=False)
+    user = models.ForeignKey(USER_MODEL, null=True, editable=False)
 
     action = models.CharField(max_length=100, editable=False)
 
@@ -33,12 +34,15 @@ class Access(MetricsModel):
 
 class UserSession(MetricsModel):
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(USER_MODEL)
     session = models.ForeignKey(Session)
 
 
 def user_logged_in_handler(sender, request, user, **kwargs):
-    UserSession.objects.get_or_create(user=user, session_id=request.session.session_key)
+    UserSession.objects.get_or_create(
+        user=user,
+        session_id=request.session.session_key,
+        )
 
 
 user_logged_in.connect(user_logged_in_handler)
