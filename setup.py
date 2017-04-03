59de57c9379c3632a0a1084ff7d45d2312bb8695
jeffrey k eliasen <jeff@jke.net>
2017-04-02 21:37:25 -1000

    versioned

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class DjangoTestAndLint(TestCommand):
 
 setup(
     name='django-autometrics-nonrel',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
