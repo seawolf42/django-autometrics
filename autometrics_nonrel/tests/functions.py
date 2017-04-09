46117f6ec6b5a7a1d8a5b62ebcd3ad75ac1f1b12
jeffrey k eliasen <jeff@jke.net>
2017-04-09 09:27:54 +0900

    helper method for passing entity lists using only IDs

--- autometrics_nonrel/tests/functions.py
+++ autometrics_nonrel/tests/functions.py
@@ -1,4 +1,5 @@
 import datetime
+import mock
 
 from django.test import TestCase
 
@@ -7,6 +8,7 @@ from django.contrib.auth.models import AnonymousUser
 from django.test.client import Client
 
 from ..functions import get_entity
+from ..functions import get_entity_id_list
 from ..functions import get_entity_list
 
 from ..models import Access
@@ -45,18 +47,21 @@ class AccessEntityTest(TestCase):
         self._test_access_state(None)
 
 
-class ListEntityTest(TestCase):
+class ListEntityIdTest(TestCase):
 
     def setUp(self):
-        self.entities = [
-            get_user_model().objects.create(username='e{0}'.format(i + 1))
-            for i in range(3)
-        ]
+        self.entity_model = 'model'
+        self.entity_ids = [123, 456, 789]
         self.session = Client().session
 
     def _test_list_state(self, expected_user):
         time_before = datetime.datetime.now()
-        get_entity_list(self.session, self.user, self.entities)
+        get_entity_id_list(
+            self.session,
+            self.user,
+            self.entity_model,
+            self.entity_ids,
+            )
         time_after = datetime.datetime.now()
         self.assertEqual(Access.objects.count(), 1)
         access = Access.objects.get()
@@ -64,9 +69,9 @@ class ListEntityTest(TestCase):
         self.assertEqual(access.session_key, self.session.session_key)
         self.assertEqual(access.user, expected_user)
         self.assertEqual(access.action, 'list')
-        self.assertEqual(access.model, self.entities[0]._meta.db_table)
+        self.assertEqual(access.model, self.entity_model)
         self.assertEqual(len(access.ids), 3)
-        self.assertEqual(access.ids, [e.pk for e in self.entities])
+        self.assertEqual(access.ids, self.entity_ids)
 
     def test_list_entities(self):
         self.user = get_user_model().objects.create(username='user')
@@ -81,9 +86,39 @@ class ListEntityTest(TestCase):
         self._test_list_state(None)
 
     def test_list_no_entites_skips_save(self):
-        get_entity_list(self.session, None, [])
+        get_entity_id_list(self.session, None, self.entity_model, [])
         self.assertEqual(Access.objects.count(), 0)
+
+
+class ListEntityTest(TestCase):
+
+    def setUp(self):
+        import autometrics_nonrel.functions as f
+        self.functions = f
+        f.get_entity_id_list = mock.MagicMock(return_value='result')
+        self.entities = [
+            get_user_model().objects.create(username='e{0}'.format(i + 1))
+            for i in range(3)
+        ]
+        self.session = 'session'
+        self.user = 'user'
+        self.entity_model = self.entities[0]._meta.db_table
 
     def test_entites_must_all_be_same_type(self):
         with self.assertRaises(AssertionError):
             get_entity_list(self.session, None, self.entities + [object()])
+
+    def test_list_no_entites_skips_helper_method_call(self):
+        get_entity_list(self.session, None, [])
+        self.functions.get_entity_id_list.assert_not_called()
+
+    def test_list_entities(self):
+        result = get_entity_list(self.session, self.user, self.entities)
+        self.assertEqual(result, 'result')
+        self.functions.get_entity_id_list.assert_called_once()
+        self.functions.get_entity_id_list.assert_called_with(
+            self.session,
+            self.user,
+            self.entity_model,
+            [e.pk for e in self.entities],
+        )
