fd16322f14b9410a3e42bb708eebc5755bbf2a61
jeffrey k eliasen <jeff@jke.net>
2017-04-11 06:55:56 -1000

    versioned

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_dependencies = [
 
 setup(
     name='django-autometrics-nonrel',
-    version='0.8',
+    version='0.9',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
