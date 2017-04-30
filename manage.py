c26ba9105a29572325fe0bbc34d089bf9033926d
jeffrey k eliasen <jeff@jke.net>
2017-04-30 10:05:17 -1000

    no actual reason to have wsgi application

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@ import os
 import sys
 
 
+wsgi_application = None
+
+
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     os.environ['DJANGAE_APP_YAML_LOCATION'] = os.path.abspath(
